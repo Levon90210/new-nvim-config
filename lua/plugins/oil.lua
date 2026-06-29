2d57ab1d23a8950a76f0081c0635faec486b60ef
Levon Shahbazyan <levonshahbazyan10@gmail.com>
2026-06-29 13:33:27 +0400

    fix some issues ig

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -7,38 +7,12 @@ return {
     { "<leader>of", function() require("oil").open_float() end, desc = "Oil: Open Floating Window" },
   },
   opts = {
-    default_file_explorer = true,
-    columns = {
-      "icon",
-    },
+    float = { border = "rounded" },
     keymaps = {
-      ["g?"] = "actions.show_help",
-      ["<CR>"] = "actions.select",
-      ["<C-s>"] = "actions.select_vsplit",
-      ["<C-v>"] = "actions.select_split",
-      ["<C-t>"] = "actions.select_tab",
-      ["<C-p>"] = "actions.preview",
-      ["<C-c>"] = "actions.close",
-      ["<C-l>"] = "actions.refresh",
-      ["-"]     = "actions.parent",
-      ["_"]     = "actions.open_cwd",
-      ["g."]    = "actions.toggle_hidden",
-      ["g\\"]   = "actions.toggle_trash",
+        ["<C-h>"] = false,
+        ["<C-l>"] = false;
+        ["<C-r>"] = "actions.refresh"
     },
-    float = {
-      padding = 2,
-      max_width = 80,
-      max_height = 0,
-      border = "rounded",
-      win_options = {
-        winblend = 0,
-      },
-    },
-    view_options = {
-      show_hidden = true,
-      is_hidden_file = function(name, bufnr)
-        return vim.startswith(name, ".")
-      end,
-    },
+    view_options = { show_hidden = true },
   },
 }
