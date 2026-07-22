d67d35362bc24171d62fea5c09a86dede7b6fb81
Levon Shahbazyan <levonshahbazyan10@gmail.com>
2026-07-22 12:14:23 +0400

    make the completion popup beautiful

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -5,82 +5,94 @@ return {
     version = "1.*",
 
     opts = {
-      keymap = {
-        preset = "default",
-        ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
-        ["<C-e>"] = { "hide", "fallback" },
-        ["<CR>"] = { "accept", "fallback" },
-        ["<Tab>"] = { "select_next", "fallback" },
-        ["<S-Tab>"] = { "select_prev", "fallback" },
-      },
-
-      appearance = {
-        use_nvim_cmp_as_default = false,
-        nerd_font_variant = "mono",
-      },
-
-      snippets = { preset = "default" },
-
-      signature = { enabled = true },
-
-      sources = {
-        default = { "lsp", "path", "snippets", "buffer" },
-        providers = {
-          lsp = {
-            name = "LSP",
-            module = "blink.cmp.sources.lsp",
-            score_offset = 100,
-          },
-          path = {
-            name = "Path",
-            module = "blink.cmp.sources.path",
-            score_offset = 80,
-          },
-          snippets = {
-            name = "Snippets",
-            module = "blink.cmp.sources.snippets",
-            score_offset = 60,
-          },
-          buffer = {
-            name = "Buffer",
-            module = "blink.cmp.sources.buffer",
-            score_offset = 0,
-          },
+        keymap = {
+            preset = "default",
+            ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
+            ["<C-e>"] = { "hide", "fallback" },
+            ["<CR>"] = { "accept", "fallback" },
+            ["<Tab>"] = { "select_next", "fallback" },
+            ["<S-Tab>"] = { "select_prev", "fallback" },
         },
-      },
 
-      completion = {
-        documentation = {
-          auto_show = true,
-          auto_show_delay_ms = 200,
+        appearance = {
+            use_nvim_cmp_as_default = false,
+            nerd_font_variant = "mono",
         },
-        menu = {
-          draw = {
-            columns = {
-              { "kind_icon" },
-              { "label", "label_description", gap = 1 },
-              { "source_name" },
+
+        snippets = { preset = "default" },
+
+        signature = { enabled = true },
+
+        sources = {
+            default = { "lsp", "path", "snippets", "buffer" },
+            providers = {
+                lsp = {
+                    name = "LSP",
+                    module = "blink.cmp.sources.lsp",
+                    score_offset = 100,
+                },
+                path = {
+                    name = "Path",
+                    module = "blink.cmp.sources.path",
+                    score_offset = 80,
+                },
+                snippets = {
+                    name = "Snippets",
+                    module = "blink.cmp.sources.snippets",
+                    score_offset = 60,
+                },
+                buffer = {
+                    name = "Buffer",
+                    module = "blink.cmp.sources.buffer",
+                    score_offset = 0,
+                },
             },
-            components = {
-              source_name = {
-                width = { max = 15 },
-                text = function(ctx)
-                  return "[" .. ctx.source_name:sub(1, 3):upper() .. "]"
-                end,
-                highlight = "BlinkCmpSource",
-              },
-            },
-          },
         },
-      },
 
-      enabled = function()
-        local filetype = vim.bo.filetype
-        local buftype = vim.bo.buftype
-        return buftype ~= "prompt"
-          and filetype ~= "TelescopePrompt"
-          and filetype ~= "fzf"
-          and vim.b.completion ~= false
-      end,
+        completion = {
+            documentation = {
+                auto_show = true,
+                auto_show_delay_ms = 200,
+            },
+            menu = {
+                draw = {
+                    columns = {
+                        { "kind_icon" },
+                        { "label", "label_description", gap = 1 },
+                        { "source_name" },
+                    },
+                    components = {
+                        source_name = {
+                            width = { max = 15 },
+                            text = function(ctx)
+                                return "[" .. ctx.source_name:sub(1, 3):upper() .. "]"
+                            end,
+                            highlight = "BlinkCmpSource",
+                        },
+                    },
+                },
+            },
+        },
+
+        windows = {
+            completion = {
+                border = "rounded",
+                scrollbar = false,
+            },
+            documentation = {
+                border = "rounded",
+                max_width = 80,
+                max_height = 20,
+            },
+        },
+
+        enabled = function()
+            local filetype = vim.bo.filetype
+            local buftype = vim.bo.buftype
+            return buftype ~= "prompt"
+            and filetype ~= "TelescopePrompt"
+            and filetype ~= "fzf"
+            and vim.b.completion ~= false
+        end,
     },
 }
